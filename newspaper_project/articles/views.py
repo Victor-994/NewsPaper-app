67c5fc883379fe504dffbb5643f9c6021d345705
Victor-994 <victoradejumo2@gmail.com>
2022-03-12 20:49:30 +0100

    first commit

---
+++ newspaper_project/articles/views.py
@@ -0,0 +1,49 @@
+from re import template
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic.edit import UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic.detail import SingleObjectMixin
+from django.views import View
+from .forms import CommentForm
+from .models import Article
+# Create your views here.
+
+
+class ArticleListView(LoginRequiredMixin, ListView):
+    model = Article
+    template_name = 'article_list.html'
+
+
+class ArticleDetailView(LoginRequiredMixin, DetailView):  # new
+    model = Article
+    template_name = 'article_detail.html'
+    
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView): 
+    model = Article
+    fields = ('title', 'body',)
+    template_name = 'article_edit.html'
+
+    def test_func(self): # new
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
+    model = Article
+    template_name = 'article_new.html'
+    fields = ('title', 'body')
+
+    def form_valid(self, form): # new
+        form.instance.author = self.request.user
+        return super().form_valid(form)
